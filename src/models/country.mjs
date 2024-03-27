c7b8705ced9414d996a46ac709d29dedeea1275f
Bhargavpatel0003 <102400296+Bhargavpatel0003@users.noreply.github.com>
2024-03-27 10:31:19 +0000

    Update country.mjs to match refactored database
    Co-authored-by: vatsal0501 <158591936+vatsal0501@users.noreply.github.com>

--- src/models/country.mjs
+++ src/models/country.mjs
@@ -1,15 +1,17 @@
-export default class Country {
+export default class Country { // class to represent a Country
     code;
     name;
     continent;
     region;
     population;
+    capital;
 
-    constructor(code, name, continent, region, population) {
+    constructor(code, name, continent, region, population, capital) {
         this.code = code;
         this.name = name;
         this.continent = continent;
         this.region = region;
         this.population = population;
+        this.capital = capital;
     }
 }
